37a9d5f8fd94d3fc69d5e156708ccd8840ac72b3
Surya-github-cloud <suryavamsepdp@gmail.com>
2025-08-02 11:26:04 +0530

    Code

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.get('/api', (req, res) => {
     res.send('API is running...');
 });
 
-app.use('/api/auth', authRoutes);
+app.use('/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/attendance', attendanceRoutes);
 app.use('/api/leaves', leaveRoutes);
